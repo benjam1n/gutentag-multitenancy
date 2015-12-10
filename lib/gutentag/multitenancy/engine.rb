f1ca108042eeb870e4c18671649c9c956cd36459
Andy Stewart <boss@airbladesoftware.com>
2015-12-10 10:40:06 +0000

    Refactor.

--- lib/gutentag/multitenancy/engine.rb
+++ lib/gutentag/multitenancy/engine.rb
@@ -1,5 +1,3 @@
-require "#{Gutentag::Engine.root}/app/models/gutentag/tag.rb"
-
 module Gutentag
   module Multitenancy
 
@@ -7,20 +5,6 @@ module Gutentag
       engine_name :gutentag_multitenancy
 
       ActiveSupport.on_load :active_record do
-        Gutentag::Tag.class_eval do
-          attr_accessible :tenant_id if ::ActiveRecord::VERSION::MAJOR == 3
-
-          scope :by_tenant_id, ->(tenant_id) { where tenant_id: tenant_id }
-
-          def self.find_by_name_and_tenant_id(name, tenant_id)
-            where(tenant_id: tenant_id).find_by_name(name)
-          end
-
-          def self.find_or_create_by_name_and_tenant_id(name, tenant_id)
-            find_by_name_and_tenant_id(name, tenant_id) || create(name: name, tenant_id: tenant_id)
-          end
-        end
-
         include Taggable
       end
     end
